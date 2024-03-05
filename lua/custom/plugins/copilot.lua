b55892528ba6611bafc230c527a1053c990ae487
AndriiZelenskyi <andriize@wix.com>
2024-03-05 13:59:35 +0200

    chore: rewamp copilot & cmp configs

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,6 +1,10 @@
 return {
 	"github/copilot.vim",
 	config = function()
-		vim.g.copilot_assume_mapped = true
+		vim.keymap.set("i", "<C-i>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
+		vim.g.copilot_no_tab_map = true
 	end,
 }
